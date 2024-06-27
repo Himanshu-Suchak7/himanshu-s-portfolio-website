71a5482fe759325846c0036789b47a0955a83da6
Himanshu-Suchak7 <himanshusuchak.hs@gmail.com>
2024-06-27 12:47:38 +0530

    added favicon, title and minor fixes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -37,7 +37,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full py-16"
+      className="bg-gradient-to-b from-gray-800 to-black w-full py-24"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
